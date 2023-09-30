64e6d9090cfc4836e868615d90c7a7aaa7eca535
y4zper <78707317+y4zper@users.noreply.github.com>
2023-09-30 18:38:47 -0500

    Update OperationController.java

--- CL1_Grupo1_Rest/api-rest-grupo1/src/main/java/pe/edu/cibertec/apirestgrupo1/controller/OperationController.java
+++ CL1_Grupo1_Rest/api-rest-grupo1/src/main/java/pe/edu/cibertec/apirestgrupo1/controller/OperationController.java
@@ -1,6 +1,7 @@
 package pe.edu.cibertec.apirestgrupo1.controller;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.apirestgrupo1.model.MensajeResponse;
 import pe.edu.cibertec.apirestgrupo1.service.OperacionesService;
 
-
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "api/operaciones")
 public class OperationController {
@@ -25,5 +26,34 @@ public class OperationController {
                     operacionesService.calcularCostoEstacionamiento(horas, minutos),HttpStatus.OK);
             }
 
+            @GetMapping("/calcularPromedio")
+            public ResponseEntity<MensajeResponse> calcularPromedioDescarte(@RequestParam Double nota1,
+                                                                    @RequestParam Double nota2,
+                                                                    @RequestParam Double nota3,
+                                                                    @RequestParam Double nota4){
+                return new ResponseEntity<>(
+                        operacionesService.calcularPromedioDescarte(nota1, nota2, nota3, nota4),HttpStatus.OK);
+            }
 
-}
+
+    @GetMapping("/verificarDNI")
+    public ResponseEntity<MensajeResponse> verificarDNI( @RequestParam Integer anoNacimiento){
+
+        return new ResponseEntity<>(
+                operacionesService.verificarDNI(anoNacimiento),HttpStatus.OK);
+    }
+
+    @GetMapping("/precio")
+    public ResponseEntity<MensajeResponse> calcularPrecio( @RequestParam Integer cantidad,
+                                                           @RequestParam Double precio){
+
+        return new ResponseEntity<>(
+                operacionesService.calcularPrecio(cantidad,precio),HttpStatus.OK);
+    }
+
+
+
+
+            }
+
+
